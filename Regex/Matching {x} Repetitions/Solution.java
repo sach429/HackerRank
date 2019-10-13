77fae92b1be1b81bd238dc4deb9ac9a5b1bc0b39
sach429 <satrajit.acharya@gmail.com>
2019-10-13 09:35:58 -0400

    Create Solution.java

---
+++ Regex/Matching {x} Repetitions/Solution.java
@@ -0,0 +1,9 @@
+public class Solution {    
+
+    public static void main(String[] args) {
+        
+        Regex_Test tester = new Regex_Test();
+        tester.checker("^[24680A-Za-z]{40}[\\s13579]{5}$"); // Use \\ instead of using \ 
+    
+    }
+}
